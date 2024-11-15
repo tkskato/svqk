3773d278518b4b3d156daafdc444689d51d761a5
tkskato <takashi.kato@monocrea.co.jp>
2024-11-15 18:15:41 +0900

    inversify-binding-decoratorsを追加

--- svqk-front/src/lib/inversify.config.ts
+++
@@ -1,9 +0,0 @@
-import { Container } from 'inversify';
-import 'reflect-metadata';
-import { MasterStore } from '$lib/arch/MasterStore';
-import { TYPES } from '$lib/types';
-
-const container = new Container();
-container.bind<MasterStore>(TYPES.MasterStore).to(MasterStore);
-
-export { container };
