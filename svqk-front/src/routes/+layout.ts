b928c92da37875b66f6d4c112fe34ce14a4e2f72
takashi.kato <takashi.kato@monocrea.co.jp>
2024-11-19 15:53:36 +0900

    types.tsの配置場所変更

--- svqk-front/src/routes/+layout.ts
+++ svqk-front/src/routes/+layout.ts
@@ -2,7 +2,7 @@ import type { LayoutLoad } from './$types';
 import { loadTranslations } from '$lib/translations';
 import { getContainer } from '$lib/arch/di/Container';
 import { LoadExecutor } from '$lib/arch/LoadExecutor';
-import { TYPES } from '$lib/arch/types';
+import { TYPES } from '$lib/arch/di/types';
 
 export const load: LayoutLoad = async ({ fetch }) => {
   const userLang = navigator.language;
