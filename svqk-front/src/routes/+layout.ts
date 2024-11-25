31351bbc919f488d524bda80fb76dd1ffc02f44c
takashi.kato <takashi.kato@monocrea.co.jp>
2024-11-25 17:39:59 +0900

    storeへの設定値反映

--- svqk-front/src/routes/+layout.ts
+++ svqk-front/src/routes/+layout.ts
@@ -3,6 +3,10 @@ import { loadTranslations } from '$lib/translations';
 import { getContainer } from '$lib/arch/di/Container';
 import { LoadExecutor } from '$lib/arch/LoadExecutor';
 import { TYPES } from '$lib/arch/di/types';
+import { issueStatuses } from '$lib/domain/issue/IssueStatusMasterStore';
+import { trackers } from '$lib/domain/issue/TrackerMasterStore';
+
+export const ssr = false;
 
 export const load: LayoutLoad = async ({ fetch }) => {
   const userLang = navigator.language;
@@ -12,5 +16,8 @@ export const load: LayoutLoad = async ({ fetch }) => {
   const loadExecutor = container.get<LoadExecutor>(TYPES.LoadExecutor);
   await loadExecutor.execute(fetch);
 
-  return {};
+  return {
+    issueStatuses,
+    trackers
+  };
 };
