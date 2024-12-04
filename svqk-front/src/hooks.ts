675633f6729447deb264dd302514eed89d558dc2
takashi.kato <takashi.kato@monocrea.co.jp>
2024-12-04 15:56:17 +0900

    fix import statements

--- svqk-front/src/hooks.ts
+++ svqk-front/src/hooks.ts
@@ -1,4 +1,5 @@
 // Modules to be registered in the DI container
 import '$lib/arch/master/LoadExecutor';
+import '$lib/arch/master/MasterStoreBase';
 import '$lib/domain/issue/IssueStatusMasterStore';
 import '$lib/domain/issue/TrackerMasterStore';
